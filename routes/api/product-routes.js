d5bc91bba961d19b7b584cd154928d54013e3d51
Archimedes <jackfranklin36@gmail.com>
2021-07-01 12:38:59 +0100

    Finished API routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  }).then((product) => res.json(product))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  }).then((productId) => res.json(productId))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
+
 });
 
 // create new product
@@ -91,6 +123,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((productId) => res.json(productId))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
+
 });
+
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,75 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ]
+  }).then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product
+      }
+    ]
+  }).then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    // .then((categoryIds) => res.status(200).json(categoryIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  })
+    .then((updateTag) => res.json(updateTag))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(() => res.json("It's gone."))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json("Whoops. Something went wrong.");
+    });
+
 });
 
 module.exports = router;
